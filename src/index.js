dfcf149be7f902ed711246d8b4707c5c8a1fa2bd
Sergey Glushakov <sergey_glushakov_web@mail.ru>
2019-11-13 22:21:30 +0300

    create first react element

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('React app');
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const element = (
+    <div>
+        <h1>My Todo List</h1>
+        <input placeholder="search"/>
+        <ul>
+            <li>Learn Reract</li>
+            <li>Build Avesome App</li>
+        </ul>
+    </div>
+);
+console.log(element);
+ReactDom.render(element,document.getElementById('root'));
