36e50e9f04a37487c6c2d10876bdc35d2968064a
Sergey Glushakov <sergey_glushakov_web@mail.ru>
2019-11-15 20:18:27 +0300

    props --> array in jsx

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,11 +1,17 @@
 import React from "react";
 import TodoListItem from './todo-list-item';
-const TodoList = () => {
+
+const TodoList = ({todos}) => {
+    const elements = todos.map((item) => {
+        return (
+            <li>
+                <TodoListItem {...item} />
+            </li>
+        );
+    });
     return (
         <ul>
-            <li><TodoListItem label="Drink Cofee"/></li>
-            <li><TodoListItem label="Build React App" important/></li>
-
+            {elements}
         </ul>
     );
 };
--- src/index.js
+++ src/index.js
@@ -8,15 +8,17 @@ import TodoList from './components/todo-list'
 
 
 const App = () => {
-    const isLoggedIn = true;
-    const loginBox = <span>Log in please</span>;
-    const welcomeBox = <span>Welcome Back</span>;
+    const todoData = [
+        {label: 'Drink Cofee', important: false},
+        {label: 'Make Awesome App', important: true},
+        {label: 'Have Lunch', important: false},
+
+    ];
     return (
         <div>
-            {isLoggedIn ? welcomeBox : loginBox}
             <AppHeader/>
             <SearchPanel/>
-            <TodoList items={['Item 1', 'Item 2']}/>
+            <TodoList todos={todoData}/>
         </div>
     );
 };
