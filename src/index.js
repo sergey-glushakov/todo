f6628f7b63d5ff87cc7f38da244e852c0095e429
Sergey Glushakov <sergey_glushakov_web@mail.ru>
2019-11-15 21:16:34 +0300

    collections and keys шÑ in JSX

--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ import TodoList from './components/todo-list'
 
 const App = () => {
     const todoData = [
-        {label: 'Drink Cofee', important: false},
-        {label: 'Make Awesome App', important: true},
-        {label: 'Have Lunch', important: false},
+        {label: 'Drink Cofee', important: false, id: 1},
+        {label: 'Make Awesome App', important: true, id: 2},
+        {label: 'Have Lunch', important: false, id: 3},
 
     ];
     return (
