344051771d480318ddc5f5ed78329719b928df75
Sergey Glushakov <sergey_glushakov_web@mail.ru>
2019-11-15 09:16:50 +0300

    Props

--- src/index.js
+++ src/index.js
@@ -1,34 +1,11 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-const TodoList = () => {
-    const items = ['Learn React', 'Build Awesome App'];
-    return (
-        <ul>
-            <li>{items[0]}</li>
-            <li>{items[1]}</li>
-        </ul>
-    );
-};
+import AppHeader from './components/app-header';
+import SearchPanel from './components/search-panel';
+import TodoList from './components/todo-list'
 
-const AppHeader = () => {
-    return <h1>My Todo List</h1>
 
-};
-
-const SearchPanel = () => {
-    const searchText = 'Type here to search'; // один из вариантов использования строкового значения переменной в качестве значения для адрибута html элемента
-    const searchStyle = {
-        fontSize: '25px',
-        color: 'red',
-        padding: '10px'
-    };
-    return <input
-        style={searchStyle} // указываем инлайн стили из объекта переменной searchStyle
-        placeholder={searchText}
-    />;
-
-};
 
 const App = () => {
     const isLoggedIn = true;
